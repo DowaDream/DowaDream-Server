1f5037f5173a661861e380a9eb136e7d1a493648
soeun <separk314@naver.com>
2023-07-28 23:52:11 +0900

    Docs: 주석 정리#1

--- user/views.py
+++ user/views.py
@@ -28,43 +28,29 @@ def google_callback(request):
     code = request.GET.get('code')
     state = "random_state"
 
-    # 1. 받은 코드로 구글에 access token 요청
     token_req = requests.post(f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}&state={state}")
-    
-    ### 1-1. json으로 변환 & 에러 부분 파싱
     token_req_json = token_req.json()
     error = token_req_json.get("error")
 
-    ### 1-2. 에러 발생 시 종료
     if error is not None:
         raise JSONDecodeError(error)
-
-    ### 1-3. 성공 시 access_token 가져오기
     access_token = token_req_json.get('access_token')
 
-    # 2. 가져온 access_token으로 이메일값을 구글에 요청
+    # 가져온 access_token으로 이메일값을 구글에 요청
     email_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
     email_req_status = email_req.status_code
-
-    ### 2-1. 에러 발생 시 400 에러 반환
     if email_req_status != 200:
         return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
     
-    ### 2-2. 성공 시 이메일 가져오기
+    # 성공 시 이메일 가져오기
     email_req_json = email_req.json()
     email = email_req_json.get('email')
-
     # return JsonResponse({'access': access_token, 'email':email})
 
     # 3. 전달받은 이메일, access_token, code를 바탕으로 회원가입/로그인
     try:
-        # 전달받은 이메일로 등록된 유저가 있는지 탐색
         user = User.objects.get(email=email)
-
-        # FK로 연결되어 있는 socialaccount 테이블에서 해당 이메일의 유저가 있는지 확인
         social_user = SocialAccount.objects.get(user=user)
-
-        # 있는데 구글계정이 아니어도 에러
         if social_user.provider != 'google':
             return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -82,12 +68,11 @@ def google_callback(request):
         return JsonResponse(accept_json)
 
     except User.DoesNotExist:
-        # 전달받은 이메일로 기존에 가입된 유저가 아예 없으면 => 새로 회원가입 & 해당 유저의 jwt 발급
+        # 새로 회원가입 & 해당 유저의 jwt 발급
         data = {'access_token': access_token, 'code': code}
         accept = requests.post(f"{BASE_URL}user/login/finish/", data=data)
         accept_status = accept.status_code
 
-        # 뭔가 중간에 문제가 생기면 에러
         if accept_status != 200:
             return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
 
