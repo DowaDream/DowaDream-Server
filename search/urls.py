3530b6fbad3cd3b5a4ec3255e6cf975ee3721359
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-08-10 22:09:22 +0900

    apiview로 리팩터링 및 데코레이터 추가

--- search/urls.py
+++ search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('keyword/', search_keyword, name='search_keyword'),
-    path('area/', search_area, name='search_area'),
-    path('registno/', search_regist_no, name='search_regist_no'),
+    path('keyword/', SearchKeywordView.as_view(), name='search_keyword'),
+    path('area/', SearchAreaView.as_view(), name='search_area'),
+    path('registno/', SearchRegistNoView.as_view(), name='search_regist_no'),
 ]
