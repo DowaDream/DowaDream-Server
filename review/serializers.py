9e33808efc760f5b0e672ee31947f8f2f732222b
soeun <separk314@naver.com>
2023-08-15 15:40:14 +0900

    Feat: 내가 한 봉사 목록에 등록된 봉사만 리뷰를 남길 수 있도록 수정#33

--- review/serializers.py
+++ review/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from program.service import get_interactions_list
 
 # 유효한 이미지 파일인지 검사
 def is_image(image):
@@ -38,7 +39,10 @@ class ReviewSerializer(serializers.ModelSerializer):
         if review.exists():
             raise serializers.ValidationError("이미 객체가 존재함")
         
-        # 
+        # 내가 한 봉사 목록에 있는 봉사만 리뷰 남길 수 있음
+        participated_prgrms = get_interactions_list(writer, 'participated')
+        if progrmRegistNo not in participated_prgrms:
+            raise serializers.ValidationError("'내가 한 봉사'에 등록된 봉사가 아님")
         return data
 
 class CommentSerializer(serializers.ModelSerializer):
