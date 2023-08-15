0fe935a88096b1de9afe50cb8283009ecaaf7906
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-08-15 22:16:59 +0900

    Chore: Program 앱 아래 추가

--- program/urls.py
+++ program/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('cheered/', CheeredGetView.as_view()),
     path('participated/', ParticipatedGetView.as_view()),
     path('clipped/', ClippedGetView.as_view()),
+    path('search/keyword/', SearchKeywordView.as_view(), name='search_keyword'),
+    path('search/area/', SearchAreaView.as_view(), name='search_area'),
+    path('search/registno/', SearchRegistNoView.as_view(), name='search_regist_no'),
 ]
--- program/views.py
+++ program/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import GenericAPIView
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.response import Response
 
 from .service import *
 from .response import *
@@ -108,3 +110,51 @@ class ClippedGetView(GenericAPIView):
         interations_list = get_interactions_list(user, 'clipped')
         res = ResponseDto(status=200, data=interations_list, msg=message["ClippedGetSuccess"])
         return responseFactory(res)
+    
+class SearchKeywordView(APIView):
+    # no permission needed
+    permission_classes = []
+
+    @swagger_auto_schema(query_serializer=SearchKeywordSerializer, responses={"200":KeywordResponseSerializer, "404":KeywordResponseSerializer})
+    def get(self, request):
+        '''
+            ## 키워드로 조회
+            - `keyword`: 검색할 키워드
+            - `actPlace`: 장소 (필수 필드 아님) Ex. 상도
+        '''
+        keyword = request.query_params.get('keyword')
+        actPlace = request.query_params.get('actPlace')
+        search_result = callByKeyword(keyword, actPlace)
+        result = searchResponseFactory(search_result)
+        if search_result is None:
+            return Response(result,status=status.HTTP_404_NOT_FOUND)
+        return Response(result,status=status.HTTP_200_OK)
+
+
+class SearchAreaView(APIView):
+    @swagger_auto_schema(query_serializer=SearchAreaSerializer, responses={"200":AreaResponseSerializer, "404":AreaResponseSerializer})
+    def get(self, request):
+        '''
+            ## 지역으로 조회
+            - `keyword`: 지역코드(구군) Ex. 3120000
+        '''
+        keyword = request.query_params.get('keyword')
+        search_result = callByArea(keyword)
+        result = searchResponseFactory(search_result)
+        if search_result is None:
+            return Response(result,status=status.HTTP_404_NOT_FOUND)
+        return Response(result,status=status.HTTP_200_OK)
+
+
+class SearchRegistNoView(APIView):
+    @swagger_auto_schema(query_serializer=SearchRegistNoSerializer, responses={"200":RegistNoResponseSerializer, "404":RegistNoResponseSerializer})
+    def get(self, request):
+        '''
+            ## 봉사ID(`progrmRegistNo`)으로 세부 봉사내용 조회
+        '''
+        keyword = request.query_params.get('keyword')
+        search_result = callByRegistNo(keyword)
+        result = searchResponseFactory(search_result)
+        if search_result is None:
+            return Response(result,status=status.HTTP_404_NOT_FOUND)
+        return Response(result,status=status.HTTP_200_OK)
