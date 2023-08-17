a294189f78d2f23b8c5e83291275c12d1d434c30
soeun <separk314@naver.com>
2023-08-17 17:01:12 +0900

    Chore: CORS ORIGIN 추가#53

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,10 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://dowadream.site"
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://api.dowadream.site']
+CSRF_TRUSTED_ORIGINS = ['https://api.dowadream.site', 'https://dowadream.site']
 
 ROOT_URLCONF = "config.urls"
 
